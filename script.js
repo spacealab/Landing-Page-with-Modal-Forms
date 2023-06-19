c7a8d48fb7434578750996937ca76f83f92150de
Ali Alipour <spacealab@gmail.com>
2023-06-19 08:28:59 +0330

    first

---
+++ script.js
@@ -0,0 +1,29 @@
+const signupModal = document.querySelector(".signup-form-wrapper");
+const loginModal = document.querySelector(".login-form-wrapper");
+const signBtn = document.querySelector(".signup-btn");
+const loginBtn = document.querySelector(".login-btn");
+
+const signupX = document.querySelector(".signup-x");
+const loginX = document.querySelector(".login-x");
+
+const formContainer = document.querySelector(".form-container");
+
+signBtn.addEventListener("click", () => {
+    signupModal.classList.add("display");
+    formContainer.classList.add("disable");
+});
+
+loginBtn.addEventListener("click", () => {
+    loginModal.classList.add("display");
+    formContainer.classList.add("disable");
+});
+
+signupX.addEventListener("click", () => {
+    signupModal.classList.remove("display");
+    formContainer.classList.remove("disable")
+});
+
+loginX.addEventListener("click", () => {
+    loginModal.classList.remove("display");
+    formContainer.classList.remove("disable");
+});
